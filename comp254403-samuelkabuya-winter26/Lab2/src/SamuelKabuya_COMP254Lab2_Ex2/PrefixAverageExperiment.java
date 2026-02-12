7356395f8724716760853a8628f870885d015d84
Samuel Kabuya <samuelkabuya408@gmail.com>
2026-02-12 16:09:32 -0500

    Completed writing the method for prefixAverage1 that uses faster method to demonstrate O(n) time complexity

--- comp254403-samuelkabuya-winter26/Lab2/src/SamuelKabuya_COMP254Lab2_Ex2/PrefixAverageExperiment.java
+++ comp254403-samuelkabuya-winter26/Lab2/src/SamuelKabuya_COMP254Lab2_Ex2/PrefixAverageExperiment.java
@@ -3,7 +3,7 @@ package SamuelKabuya_COMP254Lab2_Ex2;
 public class PrefixAverageExperiment {
 
     public static void main (String [] args){
-        int n = 1000;
+        int n = 100;
         int trial = 10;
 
         try{
@@ -13,14 +13,29 @@ public class PrefixAverageExperiment {
 
         int start = n;
 
-        System.out.println("Testing prefixAverage1, slower one");
+        System.out.println("Testing prefixAverage1, slower one with O(n^2)");
         n = start;
 
-        for (int t = 0; t < trial; t++){
+        for (int t = 0; t < trial; t++) {
             double[] data = new double[n];
 
             long startTime = System.currentTimeMillis();
             PrefixAverage.prefixAverage1(data);
+            long endTime = System.currentTimeMillis();
+
+            long elapsed = endTime - startTime;
+            System.out.println(String.format("n: %9d took %12d milliseconds", n, elapsed));
+            n *= 2;
+        }
+
+            System.out.println("Testing prefixAverage2, Faster one with O(n)");
+            n = start;
+
+            for (int t = 0; t < trial; t++){
+            double[] data = new double[n];
+
+            long startTime = System.currentTimeMillis();
+            PrefixAverage.prefixAverage2(data);
             long endTime = System.currentTimeMillis();
 
             long elapsed = endTime - startTime;
