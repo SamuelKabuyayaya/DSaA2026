d21c019916044410177b9c8e5ccab6f7e6cd8a0c
Samuel Kabuya <samuelkabuya408@gmail.com>
2026-02-12 14:41:30 -0500

    My bad, here is where I started to work on exercise 2 using the example from the StringExperiment to make it simmilar and easier to make. Last one was file creation

--- comp254403-samuelkabuya-winter26/Lab2/src/SamuelKabuya_COMP254Lab2_Ex2/PrefixAverageExperiment.java
+++ comp254403-samuelkabuya-winter26/Lab2/src/SamuelKabuya_COMP254Lab2_Ex2/PrefixAverageExperiment.java
@@ -1,4 +1,17 @@
 package SamuelKabuya_COMP254Lab2_Ex2;
 
 public class PrefixAverageExperiment {
+
+    public static void main (String [] args){
+        int n = 1000;
+        int trial = 10;
+
+        try{
+            if(args.length > 0) trial = Integer.parseInt(args[0]);
+            if(args.length > 1) n = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {}
+        int star = n;
+
+    }
+
 }
