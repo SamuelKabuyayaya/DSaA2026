48bca1a712e451ec776ab4cc6782bf8140039e85
Samuel Kabuya <samuelkabuya408@gmail.com>
2026-03-12 11:08:15 -0400

    Completed task 2. It works just fine with no problems

--- comp254403-samuelkabuya-winter26/Lab4/src/SamuelKabuya_COMP254Lab4_Ex2/StackTransfer.java
+++ comp254403-samuelkabuya-winter26/Lab4/src/SamuelKabuya_COMP254Lab4_Ex2/StackTransfer.java
@@ -1,4 +1,29 @@
 package SamuelKabuya_COMP254Lab4_Ex2;
 
 public class StackTransfer {
+
+    public static <E> void transfer(Stack<E> S, Stack<E> T){
+        while (!S.isEmpty()){
+            T.push(S.pop());
+        }
+    }
+
+    public static void main(String[] args){
+        Stack<Integer> S = new ArrayStack<>();
+        Stack<Integer> T = new ArrayStack<>();
+
+        S.push(10);
+        S.push(20);
+        S.push(30);
+
+        System.out.println("Stack S before transfer: " + S);
+        System.out.println("Stack T before transfer: " + T);
+
+        transfer(S,T);
+
+        System.out.println("After Transfer:");
+        System.out.println("Stack S" + S);
+        System.out.println("Stack T" + T);
+
+    }
 }
