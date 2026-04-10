eb0c9fc2d00a632e492f26e33525c1040d09bb5d
Samuel Kabuya <samuelkabuya408@gmail.com>
2026-04-10 10:39:52 -0400

    Task 1 completed and I imported all necessary files for task two to start working on it

---
+++ comp254403-samuelkabuya-winter26/lab6/src/SamuelKabuya_COMP254Lab6_Ex1/PerformanceTest.java
@@ -0,0 +1,24 @@
+package SamuelKabuya_COMP254Lab6_Ex1;
+
+import java.util.Random;
+
+public class PerformanceTest {
+    public static void main(String[] args) {
+        int n = 100000; // element ammount
+        double[] factors = {0.2, 0.5, 0.75, 0.9}; // different limits
+
+        System.out.println("ChainHashMap: ");
+        for (double factor : factors) {
+            ChainHashMap<Integer, Integer> map = new ChainHashMap<>(17, factor);
+            Random rnd = new Random();
+
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < n; i++) {
+                map.put(rnd.nextInt(1000000), i);
+            }
+            long endTime = System.currentTimeMillis();
+
+            System.out.println("Max Load Factor: " + factor + "  Time: " + (endTime - startTime) + " ms");
+        }
+    }
+}
